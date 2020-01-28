242f44d1de8effe0bffd1a327ac00fb33ef9d98a
zach42o1 <48109689+zach42o1@users.noreply.github.com>
2020-01-27 20:43:50 -0700

    forth commit

--- googlebooks.js
+++ googlebooks.js
@@ -1,37 +1,28 @@
-let chai = require('chai');
-let getBooks = require('../utils/googlebooks');
-let expect = chai.expect;
-let patt = /turing/i;
+const axios = require('axios')
 
-//Our parent block
-describe('Test Async Books', () => {
-    beforeEach((done) => { //Before each test we empty the database
-        done();
-    });
+module.exports = async (phrase) => {
+    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            format: 'json',
+            q: `${phrase}`
+        }
+    })
 
-    it('should return an object with list books with turing in the title', function(done) {
-        getBooks('turing').then((result) => {
-            let books = JSON.parse(result);
-            expect(books).to.be.an('object');
-            expect(books.items).to.satisfy(function(items) {
-                return items.every(function(item) {
-                    return patt.test(item.volumeInfo.title);
-                });
-            });
-           done();
-        }).catch(err => done(err));
-    });
 
-    it('should return an error with list books with "" in the title', function(done) {
-        getBooks('').then((result) => {
-            console.log(result);
-            let books = JSON.parse(result);
-            expect(books).to.be.an('object');
-            done();
-        }).catch(err => {
-                //expect(err.response).to.have.status(400);
-                expect(err.response.status).to.be.equal(400);
-                done();
-        });
-    });
-});
+    
+
+    return JSON.stringify(results.data);
+}
+
+/*  console.log(results.data);
+    console.log(results.status);
+    console.log(results.statusText);
+    console.log(results.headers);
+        const results = await axios({
+        method: 'get',
+        url: 'https://www.googleapis.com/books/v1/volumes',
+        params: {
+            format: 'json',
+            q: `${phrase}`,
+        },
+    })*/
